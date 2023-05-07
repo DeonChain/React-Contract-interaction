1c2574ec329d37be07771adfdac89822c7fc94e9
manish gupta <csedgla@gmail.com>
2023-05-07 20:01:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleStorage from './SimpleStorage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Working</h1>
+      <SimpleStorage/>
+    </div>
+  );
+}
+
+export default App;
